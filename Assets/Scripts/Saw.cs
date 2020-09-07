fc44b6702c3318ede0844b8eba3166e61e532ae4
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 15:31:15 +0500

    first

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    public bool rotationEnabled = false;
+    public float rotationsPerMinute = 0f;
+
+    void Update()
+    {
+        if (rotationEnabled)
+        {
+            transform.Rotate(0, 0, -rotationsPerMinute * Time.deltaTime, Space.Self);
+        }
+        transform.position += new Vector3(1,0,0) * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player(Clone)")
+        {
+            PhotonNetwork.Destroy(collision.gameObject);
+            PhotonNetwork.LoadLevel("Menu");
+        }
+        Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/playerManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class playerManagement : MonoBehaviour
+{
+    public PhotonView PV;
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PV.IsMine)
+        {
+            GetComponent<playerMovement>().enabled = false;
+            cam.SetActive(false);
+        }
+    }
+}
